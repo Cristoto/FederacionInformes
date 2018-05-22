92ad291fa87f38c4596be370102a74c8e915a7b0
Cristo Estévez <cristom.estevez@gmail.com>
2018-05-22 14:50:02 +0100

    Delete function file

--- php/PDF.php
+++ php/PDF.php
@@ -93,4 +93,23 @@ class PDF extends FPDF
         // Closing line
         $this->Cell($widthCells*count($header),0,'','T');
     }
+
+    /**
+     * Create a PDF with specify content
+     *
+     * @param string $title Title to the PDF
+     * @param array $header Content to the header of the table
+     * @param array $content array with the content of the table
+     * @return void
+     */
+    public static function createPDF(string $title, array $header, array $content){
+        ob_start();
+            $pdf = new PDF($title);
+            $pdf->AliasNbPages();
+            $pdf->AddPage('L');
+            $pdf->SetFont('Times','',12);
+            $pdf->loadTable($header, $content);
+            $pdf->Output();
+        ob_end_flush(); 
+    }
 }
